9c931d2421c7ef7ba4254f41602f729fd67faefa
Dan Collis-Puro <dan@collispuro.net>
2015-06-23 16:57:19 -0400

    WIP - Erubis templatted config creation

--- lib/cluster/remote_config.rb
+++ lib/cluster/remote_config.rb
@@ -1,7 +1,8 @@
 module Cluster
   class RemoteConfig
     def self.create(attributes = {})
-
+      creator = ConfigCreator.new(attributes)
+      templatted_output = creator.create
     end
   end
 end
--- spec/lib/cluster/remote_config_spec.rb
+++ spec/lib/cluster/remote_config_spec.rb
@@ -1,58 +1,53 @@
 describe Cluster::RemoteConfig do
   context '.create' do
-    it 'uses Cluster::RemoteConfigCreator to create the templatted json' do
+    it 'uses Cluster::ConfigCreator to create the templatted json' do
+      config_creator_double = double('Config creator')
+      allow(config_creator_double).to receive(:create)
+
+      allow(Cluster::ConfigCreator).to receive(:new).and_return(config_creator_double)
       attributes = {
         name: 'a name',
         cidr_block: '10.1.1.0/24',
-        variant: 'large'
+        variant: :large
       }
       described_class.create(attributes)
 
-      expect(Cluster::RemoteConfigCreator).to have_received(:new).with(attributes)
+      expect(Cluster::ConfigCreator).to have_received(:new).with(attributes)
+      expect(config_creator_double).to have_received(:create)
     end
   end
 
   context '#latest?' do
-    it 'gets the latest cluster config from the remote' do
+    it 'gets the latest cluster config from the remote'
 
-    end
+    it 'is true if our version is >= upstream and we have changes'
 
-    it 'is true if our version is >= upstream and we have changes' do
+    it 'is true if our version is >= and we do not have changes'
 
-    end
+    it 'is false if our version is <= upstream and there are changes'
 
-    it 'is true if our version is >= and we do not have changes' do
-
-    end
-
-    it 'is false if our version is <= upstream and there are changes' do
-
-    end
-
-    it 'is false if our version is <= upstream and there are no changes' do
-
-    end
+    it 'is false if our version is <= upstream and there are no changes'
   end
 
   context '#version' do
-
+    it 'gets the version from the config'
   end
 
   context '#delete' do
-    it 'deletes the remote config via the aws-sdk client' do
-
-    end
+    it 'deletes the remote config via the aws-sdk client'
+    it 'does not delete if the cluster still exists'
   end
 
   context '#changeset' do
-    it 'shows a diff against the upstream cluster config' do
+    it 'shows a diff against the upstream cluster config'
+  end
 
-    end
+  context '#changed' do
+    it 'is true if there are differences'
+    it 'is false if they are the same'
   end
 
   context '#save' do
-    it 'uses the aws-sdk client to save the latest cluster_config by name' do
-
-    end
+    it 'uses the aws-sdk client to save the latest cluster_config by name'
   end
 end
